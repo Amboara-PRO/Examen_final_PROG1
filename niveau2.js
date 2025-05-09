984c664dc1ca8824a9d5b9853dc74b1af68f277a
Mikaiah <mikaiahranaivoson@gmail.com>
2025-05-09 15:15:43 +0300

    mise a jour de niveau2.js

--- niveau2.js
+++ niveau2.js
@@ -2,30 +2,36 @@ export function countMaxBobsleigh(N,map) {
     let y = 0
     let acc = 0
     let point = 0
-    if (N<4) {
+    if (!map || !N || !map && !N) {
         return 0
     }
     else{
-        for(let i = 0 ; i<N ; i++){
-            while(y<N){
-                if (map[i][y] == '.' && map[i][y+1] == '.') {
-                    point++
-                }
-                else {
-                    point = 0
-                }
-                if (point == 3) {
-                    acc ++
-                    point = 0
-                }
-                if (y==N-1) {
-                    y=0
-                    break
-                }
-                y++
-            }
+
+        if (N<4) {
+            return 0
         }
-        return acc
+        else{
+            for(let i = 0 ; i<N ; i++){
+                while(y<N){
+                    if (map[i][y] == '.' && map[i][y+1] == '.') {
+                        point++
+                    }
+                    else {
+                        point = 0
+                    }
+                    if (point == 3) {
+                        acc ++
+                        point = 0
+                    }
+                    if (y==N-1) {
+                        y=0
+                        break
+                    }
+                    y++
+                }
+            }
+            return acc
+        }
     }
 }
 console.log(countMaxBobsleigh(5,[
