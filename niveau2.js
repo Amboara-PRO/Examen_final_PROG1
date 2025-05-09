6d85c63bfc50d56794f26e9211f323e1e5da4999
Mikaiah <mikaiahranaivoson@gmail.com>
2025-05-09 15:04:03 +0300

    ajout de niveau2.js

---
+++ niveau2.js
@@ -0,0 +1,37 @@
+export function countMaxBobsleigh(N,map) {
+    let y = 0
+    let acc = 0
+    let point = 0
+    if (N<4) {
+        return 0
+    }
+    else{
+        for(let i = 0 ; i<N ; i++){
+            while(y<N){
+                if (map[i][y] == '.' && map[i][y+1] == '.') {
+                    point++
+                }
+                else {
+                    point = 0
+                }
+                if (point == 3) {
+                    acc ++
+                    point = 0
+                }
+                if (y==N-1) {
+                    y=0
+                    break
+                }
+                y++
+            }
+        }
+        return acc
+    }
+}
+console.log(countMaxBobsleigh(5,[
+    '....X',
+    'X....',
+    '.....',
+    '..X..',
+    'X...X'
+]));
